347eda41c6ed1306348485479d69b3e4dd9a2b52
Elena1900 <oliverres@mail.ru>
2020-07-09 11:27:42 +0200

    Выдаём ошибки пользователю

--- webapp/user/forms.py
+++ webapp/user/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, EqualTo, Email
+from wtforms.validators import DataRequired, EqualTo, Email, ValidationError
+
+from webapp.user.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Имя пользователя', validators=[DataRequired()], render_kw={"class": "form-control"})
@@ -15,3 +17,13 @@ class RegistrationForm(FlaskForm):
     password2 = PasswordField('Повторите пароль', validators=[DataRequired(), EqualTo('password')], render_kw={"class": "form-control"})
     email = StringField('Электронная почта', validators=[DataRequired(), Email()], render_kw={"class": "form-control"})
     submit = SubmitField('Отправить!', render_kw={"class": "btn btn-primary"})    
+
+    def validate_username(self, username):
+        user_count = User.query.filter_by(username=username.data).count()
+        if user_count > 0:
+            raise ValidationError('Пользователь с таким именем уже существует')
+
+    def validate_email(self, email):
+        email_count = User.query.filter_by(email=email.data).count()
+        if email_count > 0:
+            raise ValidationError('Пользователь с таким имейлом уже существует')    
